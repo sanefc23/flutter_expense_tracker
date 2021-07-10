84ebd5be2fe4b58367361a0fa506561ee54695ac
sanefc23 <san.efc@gmail.com>
2021-07-10 17:20:40 -0300

    refactored + added controllers

---
+++ lib/home_page.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'transaction.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: '1',
+      title: 'Nintendo Switch',
+      amount: -69999.00,
+      date: DateTime.now(),
+      installments: 9,
+    ),
+    Transaction(
+      id: '2',
+      title: 'Pokemon Sword',
+      amount: -9300,
+      date: DateTime.now(),
+      installments: 12,
+    ),
+    Transaction(
+      id: '3',
+      title: 'Sueldo',
+      amount: 105000,
+      date: DateTime.now(),
+      installments: 0,
+    ),
+  ];
+
+  DateTime _dateTime = DateTime.now();
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+  late final dateController;
+
+  @override
+  void initState() {
+    super.initState();
+    dateController = TextEditingController(
+        text: DateFormat('dd/MM/yy').format(_dateTime).toString());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Tracker'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Card(
+            color: Colors.teal,
+            child: Container(
+              height: 150,
+              child: Center(
+                child: Text('CHART'),
+              ),
+            ),
+          ),
+          //Text Inputs
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: titleController,
+                    decoration: InputDecoration(
+                      labelText: "Title",
+                    ),
+                  ),
+                  TextField(
+                    controller: amountController,
+                    decoration: InputDecoration(
+                      labelText: "Amount",
+                    ),
+                  ),
+                  TextField(
+                    controller: dateController,
+                    onTap: () => showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2001),
+                      lastDate: DateTime(2222),
+                    ).then((date) {
+                      setState(() {
+                        dateController.text =
+                            DateFormat('dd/MM/yy').format(date!).toString();
+                      });
+                    }),
+                    decoration: InputDecoration(
+                      labelText: 'Date',
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      print(titleController.text);
+                      print(amountController.text);
+                      print(dateController.text);
+                    },
+                    child: Text('Add'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Column(
+            children: transactions
+                .map(
+                  (tx) => Container(
+                    height: 76,
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 15, vertical: 8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  tx.title,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text(
+                                  DateFormat('dd/MM/yy').format(tx.date),
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.grey[600],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            tx.installments != 0
+                                ? Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Container(
+                                        padding: EdgeInsets.all(6.0),
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(8.0),
+                                            color: Colors.grey[200]),
+                                        child: Text(
+                                          '\$ ' +
+                                              (tx.amount / tx.installments)
+                                                  .toStringAsFixed(2),
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                              fontSize: 18,
+                                              color: Colors.black54,
+                                              fontWeight: FontWeight.w600),
+                                        ),
+                                      ),
+                                      Text(
+                                        'Cuota ${tx.installments} de ${tx.installments}',
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          color: Colors.grey[600],
+                                        ),
+                                      )
+                                    ],
+                                  )
+                                : Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Container(
+                                        padding: EdgeInsets.all(6.0),
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(8.0),
+                                            color: Colors.lightGreen[100]),
+                                        child: Text(
+                                          '\$ ' + tx.amount.toStringAsFixed(2),
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                              fontSize: 18,
+                                              color: Colors.green,
+                                              fontWeight: FontWeight.w600),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+                .toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
